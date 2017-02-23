a7c77c2bc9056841290aa2578a3cc7a7703dc7ce
Jusot <mu001999@outlook.com>
2017-02-23 20:09:26 +0800

    Update NBM_demo.py

--- Naive Bayesian Model/NBM_demo.py
+++ Naive Bayesian Model/NBM_demo.py
@@ -1,3 +1,23 @@
 '''
 Naive Bayesian Model
 '''
+
+
+def loadData():
+    pass
+
+
+def loadSingleData():
+    pass
+
+
+def calculate():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
