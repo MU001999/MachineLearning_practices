6870ea600ca804581c02f4d2bcf604abca385f9e
Jusot <mu001999@outlook.com>
2017-02-27 23:11:12 +0800

    Update NBM_demo.py

--- Naive Bayesian Model/NBM_demo.py
+++ Naive Bayesian Model/NBM_demo.py
@@ -2,13 +2,23 @@
 Naive Bayesian Model
 '''
 import argparse
+import os
+import numpy
 
 
-def loadData():
+def loadData(url):
     pass
 
 
-def loadSingleData():
+def loadSingleData(url):
+    pass
+
+
+def trainNBM(args):
+    pass
+
+
+def predictData(args):
     pass
 
 
@@ -16,14 +26,23 @@ def calculate():
     pass
 
 
-def main(agrs):
-    pass
+def parseGet():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--predict',help=None,action='store_true',default=False)
+    parser.add_argument('--trainDataUrl',help='the url of the train data',type=str)
+    parser.add_argument('--trainClassUrl',help='the url of the class labels',type=str)
+    parser.add_argument('--singleDataUrl',help='the url of the data that needs to be predicted',default=None,type=str)
+    args = parser.parse_args()
+    return args
 
 
-def arg_parse():
-    pass
+def main():
+    args = parseGet()
+    if args.predict==True:
+        predictData(args)
+    else:
+        trainNBM(args)
 
 
 if __name__ == '__main__':
-    args = arg_parse()
-    main(args)
+    main()
