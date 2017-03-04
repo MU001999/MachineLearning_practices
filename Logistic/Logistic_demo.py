ee2944c7696a25f216247e01768cde86d6d8bd8d
Jusot <mu001999@outlook.com>
2017-03-04 18:36:45 +0800

    Update Logistic_demo.py

--- Logistic/Logistic_demo.py
+++ Logistic/Logistic_demo.py
@@ -2,5 +2,41 @@
 Logistic
 use Sigmoid and Gradient-descent
 '''
+import argparse
 
-def 
+
+def loadDataSet():
+  pass
+
+
+def loadSingleData():
+  pass
+
+
+def Sigmoid(inX):
+  return 1.0/(1+exp(-inX))
+
+
+def gradAscent():
+  pass
+
+
+def train():
+  pass
+
+
+def predict():
+  pass
+
+
+def main(args):
+  pass
+
+
+def parse_get():
+  pass
+
+
+if __name__ == '__main__':
+  args = parse_get()
+  main(args)
